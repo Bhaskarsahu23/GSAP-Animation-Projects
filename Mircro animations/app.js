5b0653b59690ff1847cc873178f5abceb07f9b7d
Bhaskar Sahu <bhaskarsahu2398@gmail.com>
2023-10-10 22:51:25 +0530

    add micro animations

---
+++ Mircro animations/app.js
@@ -0,0 +1,87 @@
+const tl = gsap.timeline({
+  defaults: { duration: 0.35, ease: 'Power2.easeOut' },
+});
+
+const home = document.querySelector('.home');
+const notification = document.querySelector('.notifications');
+const message = document.querySelector('.message');
+
+gsap.set('.feather', { scale: 0, transformOrigin: 'center' });
+home.addEventListener('click', () => {
+  gsap.fromTo('.home-svg', { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1 });
+  gsap.fromTo(
+    '.feather',
+    {
+      y: -5,
+      scale: 0,
+    },
+    {
+      y: 20,
+      scale: 1.8,
+      duration: 1,
+      stagger: 0.2,
+    }
+  );
+  gsap.fromTo('.right-feather', { x: 0 }, { x: 5 });
+  gsap.fromTo('.left-feather', { x: 0 }, { x: -5 });
+});
+
+gsap.set('.bell', {
+  transformOrigin: 'top center',
+});
+
+gsap.set('.ringer', {
+  transformOrigin: 'top center',
+});
+gsap.set('.wave', { opacity: 0, transformOrigin: 'bottom' });
+notification.addEventListener('click', () => {
+  gsap.fromTo(
+    '.bell',
+    {
+      rotation: 5,
+    },
+    {
+      rotation: 0,
+      duration: 2,
+      ease: 'elastic.out(10, 0.2)',
+    }
+  );
+  gsap.fromTo(
+    '.ringer',
+    {
+      rotation: 3,
+      x: 0.5,
+    },
+    {
+      rotation: 0,
+      duration: 2,
+      x: 0,
+      ease: 'elastic.out(10, 0.2)',
+    }
+  );
+  gsap.fromTo(
+    '.wave',
+    {
+      opacity: 1,
+      scale: 0,
+    },
+    {
+      scale: 1.5,
+      opacity: 0,
+      duration: 2,
+    }
+  );
+});
+
+gsap.set('.flap', { transformOrigin: 'top' });
+message.addEventListener('click', () => {
+  tl.fromTo('.messages-svg', { scale: 1 }, { scale: 0.9 });
+  tl.fromTo('.flap', { scale: 1 }, { scale: -1 }, '<50%');
+  tl.fromTo('.messages-svg', { scale: 0.9 }, { scale: 1 }, '<50%');
+  tl.fromTo(
+    '.note',
+    { y: 0, opacity: 1 },
+    { y: -40, opacity: 0, duration: 0.75 }
+  );
+  tl.to('.flap', { scale: 1 }, '<50%');
+});
